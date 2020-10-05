39d2d321aa3a51a822d1b033d0c70d4ae15a8422
TapAlexxx <lordpictures123@gmail.com>
2020-10-05 09:00:19 +0300

    MenuTask
    fix

---
+++ MenuTask/Assets/Skriptes/MenuButtonsActions.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuButtonsActions : MonoBehaviour
+{
+    [SerializeField] private GameObject _panelAboutAuthors;
+    [SerializeField] private TMP_Text _textAboutAuthors;
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void AboutAuthorsButton()
+    {
+        _panelAboutAuthors.SetActive(true);
+    }
+
+    public void ChangeTextAboutAuthors(string userText)
+    {
+        _textAboutAuthors.text = userText;
+    }
+
+    public void ClosePannelAboutAuthors()
+    {
+        _panelAboutAuthors.SetActive(false);
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
